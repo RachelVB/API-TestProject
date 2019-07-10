7024c0e225ab4f3cac907a90cdfa36b4bf6b82ee
RachelVB <rachelvanbus@hotmail.com>
2019-07-10 07:39:48 -0700

    change API implementation procedure

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 var Twit = require('twit')
  
 var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
+  consumer_key:         'xXuEzJawIjTDpe8XqBSEFCKU3',
+  consumer_secret:      'piO3XpOD6zRAi8DabIEMlmal43cBHTAX3PKfrbiqzDAdhkNrGq',
+  access_token:         '1147630775178633216-wuNlZSGJSYsWXr9CbsdZa9If21RZIo',
+  access_token_secret:  'NgCM74aBXE8Bv08iQSGefNPcZWlud34LaKNQ3ReNM1zqk',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
